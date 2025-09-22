efbf842a8c7c4049df4c38aea5aa361c1f2fb834
warneaub002 <warneaub002@alpinesd.org>
2025-09-22 11:24:54 -0600

    Update for_summation.py

--- for_summation.py
+++ for_summation.py
@@ -1,7 +1,7 @@
-number=int(input())
-x=0
-total=0
-while x<number:
-    x+=1
-    total=total+x
-print(total)
+number1=int(input())
+x1=0
+total1=0
+for x1 in range(0,number1):
+    x1+=1
+    total1=total1+x1
+print(total1)
